4720e0c9ced9666586abb358bef16407f3635c25
Ayhan Mehdiyev <mehdiyev.ayyhan@gmail.com>
2025-10-20 14:42:37 -0400

    trying out docling for step 1

--- PaperCodeSync/src/paper_parser.py
+++ PaperCodeSync/src/paper_parser.py
@@ -1,2 +1,14 @@
 # Parse given research paper into TEI XML using GROBID and return sections as such:
 # [{id, heading_path, page_start, page_end, text}]
+
+
+# let's try out Docling first 
+from docling.document_converter import DocumentConverter
+
+source = "https://arxiv.org/pdf/2206.01062" 
+converter = DocumentConverter()
+doc = converter.convert(source).document
+print(doc.export_to_markdown())
+
+
+# this outputs an .md file, but not really what we want.
